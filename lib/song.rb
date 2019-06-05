4dd14a8bff2a66399fe49b86d4d78a5113784a90
Spencer Lindemuth <lindemut@gmail.com>
2019-06-05 13:48:29 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+require_relative 'author.rb'
+require_relative 'song.rb'
+require_relative 'post.rb'
+
+
+
+
+
+
+
+class Song
+    attr_accessor :title, :artist
+    # def initialize(name)
+    #     @name = name
+    # end
+end
+
+
+
+
