18502e8cfc89286d214ea61b81a5775aaea5a223
Jason Leach <jlspursfan@gmail.com>
2019-06-06 01:00:04 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,24 +1,4 @@
 class Post
   attr_accessor :title, :author
 
-  def initialize(title)
-    @title = title
-  end
-
 end
-
-
-class Author
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-end
-
-michael_crichton = Author.new("Michael Crichton")
-travels = Post.new("Travels")
-travels.author = michael_crichton
-
-travels.author.name
